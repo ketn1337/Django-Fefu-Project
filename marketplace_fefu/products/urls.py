37ec512a4abfd58ab067c2b6898f2451e63786c0
ky0ufu <superjar@mail.ru>
2023-11-12 18:54:25 +1000

    add urls path

--- marketplace_fefu/products/urls.py
+++ marketplace_fefu/products/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
+
 urlpatterns = [
-    path('', views.index)
+    path('<slug:category_slug>/', views.category, name='category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product, name='product'),
+    path('', views.search, name="search"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- marketplace_fefu/users/models.py
+++ marketplace_fefu/users/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-class Vendor(models.Model):
 
-    user = models.OneToOneField(User, related_name='vendor', on_delete=models.CASCADE)
+class User(AbstractUser):
+
+    email = models.EmailField(max_length=254)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = PhoneNumberField()
+    
 
     def __str__(self) -> str:
-        return super().__str__()
+        return self.username
 
 
     def get_balance(self):
