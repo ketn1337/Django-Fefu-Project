c332f61eef757e801cbfff84f9f3f6572f6bf348
ky0ufu <superjar@mail.ru>
2023-11-13 23:57:54 +1000

    add auth login logout forms and views

--- marketplace_fefu/users/urls.py
+++ marketplace_fefu/users/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+appname = "users"
+
 from . import views
+
 urlpatterns = [
-    path('', views.index)
+    path("register", views.register, name="register"),
+    path('login', views.cust_login, name='login'),
+    path('logout', views.cust_logout, name='logout'),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- marketplace_fefu/users/views.py
+++ marketplace_fefu/users/views.py
@@ -1,4 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, get_user_model, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
-def index(request):
-    pass
+from .decorators import user_not_authenticated
+from .forms import UserRegistrationForm, UserLoginForm
+
+@user_not_authenticated
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            redirect('/')
+    
+    else:
+        form = UserRegistrationForm()
+
+    return render(
+        request=request,
+        template_name='users/registration.html',
+        context={'form': form})
+
+
+@login_required
+def cust_logout(request):
+    logout(request)
+    return redirect('/')
+
+
+@user_not_authenticated
+def cust_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+    form = UserLoginForm()
+
+    return render(
+        request=request,
+        template_name='users/login.html',
+        context={'form': form}
+    )
